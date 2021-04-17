3c32e000d9b8cbc3223df1b0b9ab1ce12cbeae5d
rajkumarjena <54847258+rajkumarjena@users.noreply.github.com>
2021-04-17 11:18:40 +0530

    Assignment
    Assignment on python basics

---
+++ MySecondProgmRevName.py
@@ -0,0 +1,8 @@
+"""
+Take user's First and Last name then print it in reverse Orader
+If First name is "Raj" and Last Name is "Jena", then It should print in "Jena Raj"
+"""
+
+x = input("Enter the First Name:")
+y = input("Enter last Neme:")
+print("The reverse order of First Neame: {f} and Last Name: {l} is ==> {l} {f}".format(f=x, l=y))
---
+++ SphereVolume.py
@@ -0,0 +1,7 @@
+"""
+diameter = 12 or radius is 12/2 = 6
+Formulla of sphere is 4/3 * pi * r**3
+"""
+import math
+
+print("The volume of sphere is :" + str(round(4/3 * math.pi * math.pow(6,3), 4)))
